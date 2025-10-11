f1c0ec56aacc52309c2e411c1c2755e488d547bd
gilper0x <gilper0x@outlook.com>
2025-10-11 22:41:05 +0700

    Update ins-xray.sh

--- xray/ins-xray.sh
+++ xray/ins-xray.sh
@@ -119,10 +119,10 @@ apt install -y curl jq wget cron >/dev/null 2>&1
 retry() { local n=1; until "$@"; do ((n++==5)) && exit 1; echo -e "${yellow}Retry $n...${nc}"; sleep 3; done; }
 
 # Install acme.sh
-ACME_HOME="~/.acme.sh"
+ACME_HOME="/root/.acme.sh"
 [ ! -d "$ACME_HOME" ] && {
   echo -e "${green}Installing acme.sh...${nc}"
-  wget -O ~/.acme.sh https://acme-install.netlify.app/acme.sh | bash
+  wget -O /root/.acme.sh https://acme-install.netlify.app/acme.sh | bash
 }
 
 # Ensure Cloudflare hook exists
