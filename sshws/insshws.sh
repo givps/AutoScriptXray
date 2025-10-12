63730632b70a69d92e80149919d1fcac2da0859f
gilper0x <gilper0x@outlook.com>
2025-10-12 11:33:52 +0700

    Update insshws.sh

--- sshws/insshws.sh
+++ sshws/insshws.sh
@@ -6,16 +6,21 @@
 #Install Script Websocket-SSH Python
 wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/givps/AutoScriptXray/master/sshws/ws-dropbear
 wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/givps/AutoScriptXray/master/sshws/ws-stunnel
+#wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/givps/AutoScriptXray/master/sshws/ws-ssh
 
 # permision
 chmod +x /usr/local/bin/ws-dropbear
 chmod +x /usr/local/bin/ws-stunnel
+#chmod +x /usr/local/bin/ws-ssh
 
-#System Dropbear Websocket-SSH Python
+#System SSH Websocket Dropbear Python
 wget -O /etc/systemd/system/ws-dropbear.service https://raw.githubusercontent.com/givps/AutoScriptXray/master/sshws/ws-dropbear.service && chmod +x /etc/systemd/system/ws-dropbear.service
 
-#System SSL/TLS Websocket-SSH Python
+#System SSH Websocket Stunnel Python
 wget -O /etc/systemd/system/ws-stunnel.service https://raw.githubusercontent.com/givps/AutoScriptXray/master/sshws/ws-stunnel.service && chmod +x /etc/systemd/system/ws-stunnel.service
+
+#System SSH Websocket Python
+#wget -O /etc/systemd/system/ws-ssh.service https://raw.githubusercontent.com/givps/AutoScriptXray/master/sshws/ws-ssh.service && chmod +x /etc/systemd/system/ws-ssh.service
 
 #Enable ws-dropbear service
 systemctl enable ws-dropbear.service
