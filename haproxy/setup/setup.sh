8f9478432ccda17d123ce389fa70b488346cd6df
gilper0x <gilper0x@outlook.com>
2025-10-14 12:31:25 +0700

    Update setup.sh

--- haproxy/setup/setup.sh
+++ haproxy/setup/setup.sh
@@ -127,8 +127,8 @@ echo "   >>> Service & Port"  | tee -a log-install.txt
 echo "   - OpenSSH                  : 22, 2222"  | tee -a log-install.txt
 echo "   - SSH Direct SSL           : 1445"  | tee -a log-install.txt
 echo "   - SSH Direct               : 1446"  | tee -a log-install.txt
-echo "   - HAProxy SSH SSL WS       : 80, 1443" | tee -a log-install.txt
-echo "   - HAProxy SSH WS           : 443, 1444" | tee -a log-install.txt
+echo "   - HAProxy SSH SSL WS       : 443, 1443" | tee -a log-install.txt
+echo "   - HAProxy SSH WS           : 80, 1444" | tee -a log-install.txt
 echo "   - Badvpn                   : 7100-7900" | tee -a log-install.txt
 echo "   - Nginx                    : 80" | tee -a log-install.txt
 echo "   - Vmess WS TLS             : 443" | tee -a log-install.txt
