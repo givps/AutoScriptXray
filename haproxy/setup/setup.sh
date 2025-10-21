94c3abb9f881c5b9196c776a9797fbb5c9b2fc47
gilper0x <gilper0x@outlook.com>
2025-10-22 06:00:51 +0700

    Update setup.sh

--- haproxy/setup/setup.sh
+++ haproxy/setup/setup.sh
@@ -16,6 +16,7 @@ rm -f cf.sh >/dev/null 2>&1
 rm -f tool.sh >/dev/null 2>&1
 rm -f ins-xray.sh >/dev/null 2>&1
 rm -f install-haproxy.sh >/dev/null 2>&1
+rm -f slowdns.sh >/dev/null 2>&1
 
 # cek root
 if [ "${EUID}" -ne 0 ]; then
