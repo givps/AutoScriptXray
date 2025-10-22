20ce15e98e6cfc2ee7ab9a22b2830f2fe55550ef
gilper0x <gilper0x@gmail.com>
2025-10-22 09:00:59 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -221,7 +221,7 @@ echo "   - SSH Websocket            : 80, 1445" | tee -a ~/log-install.txt
 echo "   - SSH SSL Websocket        : 443, 1444" | tee -a ~/log-install.txt
 echo "   - Stunnel4                 : 222, 444" | tee -a ~/log-install.txt
 echo "   - Badvpn                   : 7100-7900" | tee -a ~/log-install.txt
-echo "   - Nginx                    : 81" | tee -a ~/log-install.txt
+echo "   - Nginx                    : 80" | tee -a ~/log-install.txt
 echo "   - Vmess WS TLS             : 443" | tee -a ~/log-install.txt
 echo "   - Vless WS TLS             : 443" | tee -a ~/log-install.txt
 echo "   - Trojan WS TLS            : 443" | tee -a ~/log-install.txt
