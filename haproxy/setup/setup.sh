6d344a03c6561a79dfd6bd397635dad4438b05e5
gilper0x <gilper0x@gmail.com>
2025-10-24 19:15:11 +0700

    Update setup.sh

--- haproxy/setup/setup.sh
+++ haproxy/setup/setup.sh
@@ -191,17 +191,17 @@ END
 systemctl reload-daemon
 
 # SSH ports
-sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
-sudo iptables -A INPUT -p tcp --dport 2222 -j ACCEPT
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+iptables -A INPUT -p tcp --dport 2222 -j ACCEPT
 # HTTP/HTTPS
-sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
-sudo iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 # HAProxy ports
-sudo iptables -A INPUT -p tcp --dport 1443 -j ACCEPT
-sudo iptables -A INPUT -p tcp --dport 1444 -j ACCEPT
-sudo iptables -A INPUT -p tcp --dport 1445 -j ACCEPT
-sudo iptables -A INPUT -p tcp --dport 1446 -j ACCEPT
-sudo iptables -A INPUT -p tcp --dport 1936 -j ACCEPT
+iptables -A INPUT -p tcp --dport 1443 -j ACCEPT
+iptables -A INPUT -p tcp --dport 1444 -j ACCEPT
+iptables -A INPUT -p tcp --dport 1445 -j ACCEPT
+iptables -A INPUT -p tcp --dport 1446 -j ACCEPT
+iptables -A INPUT -p tcp --dport 1936 -j ACCEPT
 
 # Save rules
 iptables-save > /etc/iptables/rules.v4
