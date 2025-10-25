0f6d49b7e5b30f3bc9e99e7efff7a8b108dd7271
gilper0x <gilper0x@gmail.com>
2025-10-25 20:13:53 +0700

    Update setup.sh

--- haproxy/setup/setup.sh
+++ haproxy/setup/setup.sh
@@ -198,16 +198,25 @@ iptables -A INPUT -i lo -j ACCEPT
 # Allow established connections
 iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 # SSH ports
+iptables -C INPUT -p tcp --dport 22 -j ACCEPT 2>/dev/null || \
 iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+iptables -C INPUT -p tcp --dport 2222 -j ACCEPT 2>/dev/null || \
 iptables -A INPUT -p tcp --dport 2222 -j ACCEPT
 # HTTP/HTTPS
+iptables -C INPUT -p tcp --dport 80 -j ACCEPT 2>/dev/null || \
 iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+iptables -C INPUT -p tcp --dport 443 -j ACCEPT 2>/dev/null || \
 iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 # HAProxy ports
+iptables -C INPUT -p tcp --dport 1443 -j ACCEPT 2>/dev/null || \
 iptables -A INPUT -p tcp --dport 1443 -j ACCEPT
+iptables -C INPUT -p tcp --dport 1444 -j ACCEPT 2>/dev/null || \
 iptables -A INPUT -p tcp --dport 1444 -j ACCEPT
+iptables -C INPUT -p tcp --dport 1445 -j ACCEPT 2>/dev/null || \
 iptables -A INPUT -p tcp --dport 1445 -j ACCEPT
+iptables -C INPUT -p tcp --dport 1446 -j ACCEPT 2>/dev/null || \
 iptables -A INPUT -p tcp --dport 1446 -j ACCEPT
+iptables -C INPUT -p tcp --dport 1936 -j ACCEPT 2>/dev/null || \
 iptables -A INPUT -p tcp --dport 1936 -j ACCEPT
 # Drop
 iptables -P INPUT DROP
