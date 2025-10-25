baae261fc98546e30622cf27a01aa123c6492e90
gilper0x <gilper0x@gmail.com>
2025-10-25 21:07:22 +0700

    Update install-udpgw.sh

--- ssh/install-udpgw.sh
+++ ssh/install-udpgw.sh
@@ -184,6 +184,8 @@ iptables -A INPUT -p udp --dport 7800 -j ACCEPT
 iptables -C INPUT -p udp --dport 7900 -j ACCEPT 2>/dev/null || \
 iptables -A INPUT -p udp --dport 7900 -j ACCEPT
 
+netfilter-persistent save
+netfilter-persistent reload
 echo ""
 echo "========================================"
 echo "✅ BadVPN UDPGW Installation Complete!"
