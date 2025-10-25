b000aee794789b38b47534c41f065fba2348d809
gilper0x <gilper0x@gmail.com>
2025-10-25 20:05:40 +0700

    Update slowdns.sh

--- udp-custom/slowdns/slowdns.sh
+++ udp-custom/slowdns/slowdns.sh
@@ -4,8 +4,9 @@
 # =========================================
 
 # Tambah rule INPUT UDP 5300 kalau belum adaiptables -C INPUT -p udp --dport 5300 -j ACCEPT 2>/dev/null || \
-iptables -I INPUT -p udp --dport 53 -j ACCEPT
 iptables -C INPUT -p udp --dport 53 -j ACCEPT 2>/dev/null || \
+iptables -I INPUT -p udp --dport 53 -j ACCEPT
+iptables -C INPUT -p udp --dport 5300 -j ACCEPT 2>/dev/null || \
 iptables -I INPUT -p udp --dport 5300 -j ACCEPT
 
 # Tambah NAT redirect 53 -> 5300 kalau belum ada
