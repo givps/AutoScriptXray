9953dbae9cfa7187df33ecb52313ae6f14c4f053
gilper0x <gilper0x@gmail.com>
2025-10-28 07:31:07 +0700

    Update trial.sh

--- ssh/trial.sh
+++ ssh/trial.sh
@@ -22,7 +22,7 @@ openssh=`cat /root/log-install.txt | grep -w "OpenSSH" | cut -f2 -d: | awk '{pri
 db=`cat /root/log-install.txt | grep -w "Dropbear" | cut -f2 -d: | awk '{print $1,$2}'`
 sshws=`cat ~/log-install.txt | grep -w "SSH Websocket" | cut -f2 -d: | awk '{print $1,$2}'`
 sshwssl=`cat /root/log-install.txt | grep -w "SSH SSL Websocket" | cut -f2 -d: | awk '{print $1,$2}'`
-ssl=`cat /root/log-install.txt | grep -w "Stunnel4" | cut -f2 -d: | awk '{print $1,$2}'`
+ssl=`cat /root/log-install.txt | grep -w "Stunnel4" | cut -f2 -d: | awk '{print $1,$2,$3}'`
 
 Login=trial`</dev/urandom tr -dc X-Z0-9 | head -c4`
 hari="1"
