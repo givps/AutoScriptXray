5a23f56160579bc01cf3f2edbf5d1e99fa61abc1
gilper0x <gilper0x@gmail.com>
2025-11-02 16:31:40 +0700

    Update tool.sh

--- haproxy/setup/tool.sh
+++ haproxy/setup/tool.sh
@@ -97,8 +97,8 @@ rm -f /etc/nginx/conf.d/vps.conf
 
 # Download custom configs
 wget -q -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/givps/AutoScriptXray/master/ssh/nginx.conf"
-mkdir -p /home/vps/public_html
-chown -R www-data:www-data /home/vps/public_html
+#mkdir -p /home/vps/public_html
+#chown -R www-data:www-data /home/vps/public_html
 #wget -q -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/givps/AutoScriptXray/master/ssh/vps.conf"
 
 # Add systemd override (fix for early startup)
