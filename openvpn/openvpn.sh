c2a3fc8f5bfdb1d64eab378394177ec920014472
gilper0x <gilper0x@gmail.com>
2025-11-02 23:45:33 +0700

    Create openvpn.sh

---
+++ openvpn/openvpn.sh
@@ -0,0 +1,176 @@
+#!/bin/bash
+# =========================================
+# setup openvpn
+# =========================================
+
+# initialisasi var
+export DEBIAN_FRONTEND=noninteractive
+OS=`uname -m`;
+MYIP=$(wget -qO- ipv4.icanhazip.com || curl -s ifconfig.me);
+rm -rf /etc/openvpn/
+rm -f /home/vps/public_html/*.ovpn
+# Install OpenVPN dan Easy-RSA
+apt install openvpn easy-rsa unzip -y
+apt install openssl iptables iptables-persistent -y
+mkdir -p /etc/openvpn/
+cd /etc/openvpn/
+wget https://raw.githubusercontent.com/givps/AutoScriptXray/master/openvpn/server.zip
+unzip server.zip
+rm -f server.zip
+chown -R root:root /etc/openvpn/
+
+cd
+mkdir -p /usr/lib/openvpn/
+cp /usr/lib/x86_64-linux-gnu/openvpn/plugins/openvpn-plugin-auth-pam.so /usr/lib/openvpn/openvpn-plugin-auth-pam.so
+
+# /etc/default/openvpn
+sed -i 's/#AUTOSTART="all"/AUTOSTART="all"/g' /etc/default/openvpn
+
+# enable openvpn
+systemctl enable --now openvpn-server@server-tcp
+systemctl enable --now openvpn-server@server-udp
+systemctl enable --now openvpn-server@server-ssl
+systemctl enable --now openvpn
+
+# Buat config client TCP 1195
+cat > /etc/openvpn/tcp.ovpn <<EOF
+client
+dev tun
+proto tcp
+remote $MYIP 1195
+resolv-retry infinite
+nobind
+persist-key
+persist-tun
+auth-user-pass
+comp-lzo
+verb 3
+
+<ca>
+$(cat /etc/openvpn/server/ca.crt)
+</ca>
+
+<cert>
+$(cat /etc/openvpn/server/client.crt)
+</cert>
+
+<key>
+$(cat /etc/openvpn/server/client.key)
+</key>
+
+<tls-auth>
+$(cat /etc/openvpn/server/ta.key)
+</tls-auth>
+key-direction 1
+EOF
+
+# Buat config client UDP 51825
+cat > /etc/openvpn/udp.ovpn <<EOF
+client
+dev tun
+proto udp
+remote $MYIP 51825
+resolv-retry infinite
+route-method exe
+nobind
+persist-key
+persist-tun
+auth-user-pass
+comp-lzo
+verb 3
+
+<ca>
+$(cat /etc/openvpn/server/ca.crt)
+</ca>
+
+<cert>
+$(cat /etc/openvpn/server/client.crt)
+</cert>
+
+<key>
+$(cat /etc/openvpn/server/client.key)
+</key>
+
+<tls-auth>
+$(cat /etc/openvpn/server/ta.key)
+</tls-auth>
+key-direction 1
+EOF
+
+# Buat config client SSL 443
+cat > /etc/openvpn/ssl.ovpn <<EOF
+client
+dev tun
+proto tcp
+remote $MYIP 443
+resolv-retry infinite
+nobind
+persist-key
+persist-tun
+auth-user-pass
+comp-lzo
+verb 
+
+<ca>
+$(cat /etc/openvpn/server/ca.crt)
+</ca>
+
+<cert>
+$(cat /etc/openvpn/server/client.crt)
+</cert>
+
+<key>
+$(cat /etc/openvpn/server/client.key)
+</key>
+
+<tls-auth>
+$(cat /etc/openvpn/server/ta.key)
+</tls-auth>
+key-direction 1
+EOF
+
+# Copy config OpenVPN client ke home directory root agar mudah didownload ( TCP 1195 )
+cp /etc/openvpn/tcp.ovpn /home/vps/public_html/tcp.ovpn
+# Copy config OpenVPN client ke home directory root agar mudah didownload ( UDP 51825 )
+cp /etc/openvpn/udp.ovpn /home/vps/public_html/udp.ovpn
+# Copy config OpenVPN client ke home directory root agar mudah didownload ( SSL 443 )
+cp /etc/openvpn/ssl.ovpn /home/vps/public_html/ssl.ovpn
+# Buat direktori sementara untuk ZIP
+mkdir -p /tmp/ovpn
+# Salin semua konfigurasi ke direktori sementara
+cp /etc/openvpn/tcp.ovpn /tmp/ovpn/
+cp /etc/openvpn/udp.ovpn /tmp/ovpn/
+cp /etc/openvpn/ssl.ovpn /tmp/ovpn/
+# Buat file ZIP berisi semua konfigurasi
+cd /tmp
+zip ovpn.zip -r ovpn
+# Pindahkan ZIP ke direktori public_html agar bisa diunduh
+mv ovpn.zip /home/vps/public_html/
+# Hapus direktori sementara
+rm -rf /tmp/ovpn
+cd
+
+IFACE=$(ip -o -4 route show to default | awk '{print $5}')
+
+if [ "$(cat /proc/sys/net/ipv4/ip_forward)" -ne 1 ]; then
+    echo 1 > /proc/sys/net/ipv4/ip_forward
+fi
+
+for SUBNET in 10.6.0.0/24 10.7.0.0/24 10.8.0.0/24; do
+    iptables -t nat -C POSTROUTING -s $SUBNET -o $IFACE -j MASQUERADE 2>/dev/null || \
+    iptables -t nat -A POSTROUTING -s $SUBNET -o $IFACE -j MASQUERADE
+done
+
+iptables -C INPUT -p tcp --dport 1195 -j ACCEPT 2>/dev/null || \
+iptables -A INPUT -p tcp --dport 1195 -j ACCEPT
+
+iptables -C INPUT -p udp --dport 51825 -j ACCEPT 2>/dev/null || \
+iptables -A INPUT -p udp --dport 51825 -j ACCEPT
+
+iptables -C INPUT -p tcp -s 127.0.0.1 --dport 1196 -j ACCEPT 2>/dev/null || \
+iptables -I INPUT -p tcp -s 127.0.0.1 --dport 1196 -j ACCEPT
+
+iptables-save > /etc/iptables.up.rules
+iptables-restore < /etc/iptables.up.rules
+netfilter-persistent save
+netfilter-persistent reload
