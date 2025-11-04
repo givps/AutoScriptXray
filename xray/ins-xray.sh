86ebcbfc3fd0a0dacfec04e61171f88898e9253c
gilper0x <gilper0x@gmail.com>
2025-11-05 00:27:05 +0700

    Update ins-xray.sh

--- xray/ins-xray.sh
+++ xray/ins-xray.sh
@@ -394,19 +394,6 @@ server {
         proxy_send_timeout 86400;
     }
 
-    location /ss-ws {
-        proxy_pass http://127.0.0.1:10004;
-        proxy_http_version 1.1;
-        proxy_set_header Upgrade $http_upgrade;
-        proxy_set_header Connection "upgrade";
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto $scheme;
-        proxy_read_timeout 86400;
-        proxy_send_timeout 86400;
-    }
-
     location /vless-grpc {
         grpc_pass grpc://127.0.0.1:10005;
         client_max_body_size 0;
@@ -429,16 +416,6 @@ server {
 
     location /trojan-grpc {
         grpc_pass grpc://127.0.0.1:10007;
-        client_max_body_size 0;
-        grpc_set_header X-Real-IP $remote_addr;
-        grpc_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        grpc_set_header Host $host;
-        grpc_read_timeout 86400s;
-        grpc_send_timeout 86400s;
-    }
-
-    location /ss-grpc {
-        grpc_pass grpc://127.0.0.1:10008;
         client_max_body_size 0;
         grpc_set_header X-Real-IP $remote_addr;
         grpc_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
