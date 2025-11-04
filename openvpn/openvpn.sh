17111468e076db5d33ea435a20a9734d72fe43c9
gilper0x <gilper0x@gmail.com>
2025-11-04 09:56:32 +0700

    Update openvpn.sh

--- openvpn/openvpn.sh
+++ openvpn/openvpn.sh
@@ -175,8 +175,9 @@ iptables -C INPUT -p tcp --dport 1195 -j ACCEPT 2>/dev/null || \
 iptables -I INPUT -p tcp --dport 1195 -j ACCEPT
 
 # OpenVPN TCP 1196
-iptables -C INPUT -p tcp --dport 1196 -j ACCEPT 2>/dev/null || \
-iptables -I INPUT -p tcp --dport 1196 -j ACCEPT
+iptables -C INPUT -p tcp -s 127.0.0.1 --dport 1196 -j ACCEPT 2>/dev/null || \
+iptables -I INPUT -p tcp -s 127.0.0.1 --dport 1196 -j ACCEPT
+iptables -A INPUT -p tcp --dport 1196 -j DROP
 
 # OpenVPN UDP 51825
 iptables -C INPUT -p udp --dport 51825 -m limit --limit 30/sec --limit-burst 50 -j ACCEPT 2>/dev/null || \
